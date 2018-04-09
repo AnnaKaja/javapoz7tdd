dde232cb78c5ee12c9478bd546b4927da0b6b21c
anna kaja <anna_kaja@op.pl>
2018-04-09 19:12:47 +0200

    introduced unit testing for calculator class

---
+++ src/main/java/com/sda/calculator/Calculator.java
@@ -0,0 +1,21 @@
+package com.sda.calculator;
+
+public class Calculator {
+    public int sum ( int a, int b){
+        return a + b;
+    }
+    public int substract (int a, int b){
+        return a - b;
+
+    }
+
+    public int multiply (int a, int b){
+        return a * b;
+
+    }
+
+    public int divide (int a, int b){
+        return a / b;
+
+    }
+}
