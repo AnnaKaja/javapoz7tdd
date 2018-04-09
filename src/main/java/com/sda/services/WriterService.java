a87065cf0f69bc4e0815ebc4f698859d96a40c31
anna kaja <anna_kaja@op.pl>
2018-04-09 20:48:14 +0200

    blank name functionality

--- src/main/java/com/sda/services/WriterService.java
+++ src/main/java/com/sda/services/WriterService.java
@@ -1,32 +1,33 @@
 package com.sda.services;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class WriterService {
     public String write(String name) {
         return prefix(name) + content(name) + suffix(name);
-
     }
 
     private String prefix(String name) {
         return isCapitalizedName(name) ? "HELLO, " : "Hello, ";
     }
 
-    private String content(String name) {
-        return name == null ? "my friend" : name;
+    public static void main(String[] args) {
+        WriterService writerService = new WriterService();
+        String write = writerService.write("");
 
+
+        System.out.println(write);
     }
 
     private String suffix(String name) {
         return isCapitalizedName(name) ? "!" : ".";
     }
 
-    private boolean isCapitalizedName(String name) {
-        return name != null && name.toUpperCase().equals(name);
-
-
-        //  String nameToDisplay = name == null ? "my friend" : name;
-        //  return "Hello, " + nameToDisplay + ".";
-
+    private String content(String name) {
+        return StringUtils.isBlank(name) ? "my friend" : name;
     }
 
-
+    private boolean isCapitalizedName(String name) {
+        return StringUtils.isNotBlank(name) && name.toUpperCase().equals(name);
+    }
 }
