dd9682aea0598ae271eef5c1126e41602a469d2b
anna kaja <anna_kaja@op.pl>
2018-04-10 19:33:25 +0200

    calculation of:
    - single element
    - multiple elements
    - multiple elements with whitespaces

--- src/test/java/com/sda/service/StringCalculatorTest.java
+++ src/test/java/com/sda/service/StringCalculatorTest.java
@@ -15,9 +15,39 @@ public class StringCalculatorTest {
     }
 
     @Test
+    public void testSingleNumber() {
+        //given
+        String value = "5";
+        //when
+        int result = calculator.calculate(value);
+        //then
+        Assert.assertEquals(5, result);
+    }
+
+    @Test
+    public void testMultipleNumbers() {
+        //given
+        String value = "1;2;3;4;5;6";
+        //when
+        int result = calculator.calculate(value);
+        //then
+        Assert.assertEquals(21, result);
+    }
+
+    @Test
+    public void testMultipleNumbersWithWhitespaces() {
+        //given
+        String value = "1;2     ;3 ;4 ;5\n;6";
+        //when
+        int result = calculator.calculate(value);
+        //then
+        Assert.assertEquals(21, result);
+    }
+
+    @Test
     public void testCalculationForBlankValue() {
         //given
-        String value = null;
+        String value = "";
         //when
         int result = calculator.calculate(value);
         //then
@@ -36,15 +66,10 @@ public class StringCalculatorTest {
 
     @Test
     public void testSimpleCalculation() {
-        //given
-        StringCalculator calculator = new StringCalculator();
-
-        //then
         Assert.assertEquals(5, calculator.calculate("2;3"));
         Assert.assertEquals(6, calculator.calculate("1;5"));
         Assert.assertEquals(17, calculator.calculate("8;9"));
         Assert.assertEquals(35, calculator.calculate("15;20"));
     }
-
 }
 
