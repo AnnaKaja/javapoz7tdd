0a81119322c861c7f7ddbdbfe342e523e7cde5c8
anna kaja <anna_kaja@op.pl>
2018-04-12 19:42:30 +0200

    cucumber warmup

---
+++ src/main/java/com/sda/services/bookstore/Bookstore.java
@@ -0,0 +1,30 @@
+package com.sda.services.bookstore;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bookstore {
+
+    private final List<Book> books;
+
+    public Bookstore() {
+        this.books = new ArrayList<>();
+
+    }
+
+    public void add(Book book) {
+        books.add(book);
+    }
+
+    public void updateTitle(Book book, String title) {
+        Book bookInBookstore = books.get(books.indexOf(book));
+        bookInBookstore.setTitle(title);
+
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+
+}
